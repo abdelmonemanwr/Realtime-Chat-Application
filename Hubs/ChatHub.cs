013485c4c5b9002a29475a8f3a9ef77d5dacdaeb
abdelmonemanwr <abdelmonemanwr7777@gmail.com>
2024-05-08 15:01:38 +0300

    Add project files.

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using SignalrD1.Models;
+namespace SignalrD1.Hubs
+{
+    /*public interface IChatHub { }*/
+
+    public class ChatHub:Hub
+    {
+        ITIContext db;
+        public ChatHub(ITIContext db)
+        {
+            this.db = db; 
+        }
+
+        #region day1
+        public async Task sendmessage(chatmessage mess)
+        {
+            //broadcasting to all online clients
+            await Clients.All.SendAsync("newmessage",mess);
+            
+            await db.chatmessages.AddAsync(mess);
+            await db.SaveChangesAsync();
+        }
+        #endregion
+
+
+        public async Task joingroup(string groupname, string username)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupname);
+            await Clients.OthersInGroup(groupname)
+                         .SendAsync("newmemberjoinednotification", username, groupname);
+        }
+        
+        public async Task sendmessagetogroup(string groupname, chatmessage chatmessage)
+        {
+            await Clients.Group(groupname).SendAsync("sendmessagetomygroup",groupname, chatmessage);
+        }
+    }
+}
---
+++ Migrations/20240508092342_v1.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SignalrD1.Migrations
+{
+    /// <inheritdoc />
+    public partial class v1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "chatmessage",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    messagetxt = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    username = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_chatmessage", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "chatmessage");
+        }
+    }
+}
